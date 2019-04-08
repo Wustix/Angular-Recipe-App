036d24157a77476ad45b29e2ca6144977cf58cbd
unknown <wroehrman@yahoo.com>
2019-04-07 22:08:19 -0500

    compnent updates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 
-    'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjDyu7U5r7hAhVEZN8KHWXSCkAQjRx6BAgBEAU&url=https%3A%2F%2Fitunes.apple.com%2Fus%2Fapp%2Fmy-recipe-book-organizer%2Fid375811859%3Fmt%3D8&psig=AOvVaw0xXdQ1WiBtjRW-IB85JKUJ&ust=1554754677209753')
+    'https://www.getelastic.com/wp-content/uploads/shwans-recipe.jpg')
   ];
   constructor() { }
 
